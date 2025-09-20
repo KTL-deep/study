1766f4489aeaadfa8555520269f49ba54427b9ca
Kortim_PC <timlilkor@gmail.com>
2025-09-20 22:40:26 +0300

    1

--- 1 sprint.py
+++ 1 sprint.py
@@ -63,32 +63,67 @@
 #           f'Молодец!')  # Здесь подготовьте строку для вывода
 # print(output)
 # #-------------------------------------------------
-weight = 75 # Вес
-height = 175 # Рост
-steps = 8462 # Количество пройденных за день шагов
-hours = 2 # Время движения в часах
-len_step_m = 0.65 # Длина одного шага в метрах
-transfer_coeff = 1000 # Коэффициент перевода значения расстояния из метров в километры
-
-dist =  (steps * len_step_m) / transfer_coeff # Напишите формулу расчёта
-
-mean_speed = dist / hours
-minutes = hours * 60
-
-spent_calories = (0.035*weight + (mean_speed**2 / height) * 0.029*weight) * minutes
-
-
-if dist >= 6.5:
-    motivations = 'Отличный результат! Цель достигнута!'
-elif dist >= 3.9:
-    motivations = 'Неплохо! День был продуктивным.'
-elif dist >=2:
-    motivations = 'Маловато, но завтра наверстаем!'
-else:
-    motivations = 'Лежать тоже полезно. Главное — участие, а не победа!'
-
-output = (f'''Сегодня вы прошли {steps} шагов.
-Пройденная дистанция {dist:.1f} км.
-Вы сожгли {spent_calories:.2f} ккал.
-{motivations}''')  # Здесь подготовьте строку для вывода
-print(output)
+# weight = 75 # Вес
+# height = 175 # Рост
+# steps = 8462 # Количество пройденных за день шагов
+# hours = 2 # Время движения в часах
+# len_step_m = 0.65 # Длина одного шага в метрах
+# transfer_coeff = 1000 # Коэффициент перевода значения расстояния из метров в километры
+#
+# dist =  (steps * len_step_m) / transfer_coeff # Напишите формулу расчёта
+#
+# mean_speed = dist / hours
+# minutes = hours * 60
+#
+# spent_calories = (0.035*weight + (mean_speed**2 / height) * 0.029*weight) * minutes
+#
+#
+# if dist >= 6.5:
+#     motivations = 'Отличный результат! Цель достигнута!'
+# elif dist >= 3.9:
+#     motivations = 'Неплохо! День был продуктивным.'
+# elif dist >=2:
+#     motivations = 'Маловато, но завтра наверстаем!'
+# else:
+#     motivations = 'Лежать тоже полезно. Главное — участие, а не победа!'
+#
+# output = (f'''Сегодня вы прошли {steps} шагов.
+# Пройденная дистанция {dist:.1f} км.
+# Вы сожгли {spent_calories:.2f} ккал.
+# {motivations}''')  # Здесь подготовьте строку для вывода
+# print(output)
+# #-------------------------------------------------
+# numbers = [i ** 2 for i in range(1, 11)]
+# print(numbers)
+# # #-------------------------------------------------
+# movie_ratings = [4.7, 5.0, 4.3, 3.8]
+# new_movie_ratings = [
+#    rating + 0.2 if rating < 4.9 else rating for rating in movie_ratings
+# ]
+# print(new_movie_ratings)
+# # Вывод в терминал: [4.9, 5.0, 4.5, 4.0]
+# #-------------------------------------------------
+# week = [
+#     'Понедельник', 'Вторник', 'Среда',
+#     'Четверг', 'Пятница', 'Суббота', 'Воскресенье'
+# ]
+#
+# mon, tue, wed, thu, fri, sat, sun = week
+# print(wed)
+# print(id(wed))
+# # Вывод в терминал: Среда
+# #-------------------------------------------------
+# force_words = ['сила', 'пребудет', 'с', 'тобой', 'Да']
+# print(id(force_words))
+#
+# # Место для вашего кода
+# first = force_words.pop(-1)
+# last = force_words.pop(0)
+# force_words.insert(0, first)
+# force_words.append(last)
+#
+# print(force_words)
+#
+# # Убедимся, что это тот же объект, что и в начале программы
+# print(id(force_words))
+# #-------------------------------------------------
