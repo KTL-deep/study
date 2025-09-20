5ac8e8d1a326d59b7a5bb64252f097708e4a2ce1
Kortim_PC <timlilkor@gmail.com>
2025-09-20 19:07:07 +0300

    1

--- 1 sprint.py
+++ 1 sprint.py
@@ -45,3 +45,50 @@
 # print(status_string.title())
 # print(status_string.swapcase())
 # #-------------------------------------------------
+# weight = 75 # Вес
+# height = 175 # Рост
+# steps = 8462 # Количество пройденных за день шагов
+# hours = 2 # Время движения в часах
+# len_step_m = 0.65 # Длина одного шага в метрах
+# transfer_coeff = 1000 # Коэффициент перевода значения расстояния из метров в километры
+#
+# dist = steps * len_step_m / transfer_coeff # Напишите формулу расчёта
+#
+# mean_speed = dist / hours
+# minutes = hours * 60
+#
+# spent_calories = (0.035*weight + (mean_speed**2 / height) * 0.029*weight) * minutes
+#
+# output = (f'Вы прошли {dist} км и потратили {spent_calories:.1f} калорий, '
+#           f'Молодец!')  # Здесь подготовьте строку для вывода
+# print(output)
+# #-------------------------------------------------
+weight = 75 # Вес
+height = 175 # Рост
+steps = 8462 # Количество пройденных за день шагов
+hours = 2 # Время движения в часах
+len_step_m = 0.65 # Длина одного шага в метрах
+transfer_coeff = 1000 # Коэффициент перевода значения расстояния из метров в километры
+
+dist =  (steps * len_step_m) / transfer_coeff # Напишите формулу расчёта
+
+mean_speed = dist / hours
+minutes = hours * 60
+
+spent_calories = (0.035*weight + (mean_speed**2 / height) * 0.029*weight) * minutes
+
+
+if dist >= 6.5:
+    motivations = 'Отличный результат! Цель достигнута!'
+elif dist >= 3.9:
+    motivations = 'Неплохо! День был продуктивным.'
+elif dist >=2:
+    motivations = 'Маловато, но завтра наверстаем!'
+else:
+    motivations = 'Лежать тоже полезно. Главное — участие, а не победа!'
+
+output = (f'''Сегодня вы прошли {steps} шагов.
+Пройденная дистанция {dist:.1f} км.
+Вы сожгли {spent_calories:.2f} ккал.
+{motivations}''')  # Здесь подготовьте строку для вывода
+print(output)
